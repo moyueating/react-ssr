0af5b5925feac819c777c6b4f4333831a69d7166
zkj <whutzkj@163.com>
2018-12-07 18:52:49 +0800

    update

--- client/views/topic-list/index.js
+++ client/views/topic-list/index.js
@@ -4,6 +4,7 @@ import {
   observer
 } from 'mobx-react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 @inject('appStore')
 @observer
@@ -13,12 +14,35 @@ export default class TopicList extends Component {
     alert('clicked!')
   }
 
+  componentDidMount() {
+    axios.get('https://cnodejs.org/api/v1/topics').then(res => {
+      console.log(res)
+    })
+  }
+
+  login = () => {
+    axios.post('https://cnodejs.org/api/v1/accesstoken', {
+      accesstoken: ''
+    }).then(res => {
+      console.log(res)
+    })
+  }
+
+  mark = () => {
+    axios.post('https://cnodejs.org/api/v1/message/mark_all', {
+      accesstoken: ''
+    }).then(res => {
+      console.log(res)
+    })
+  }
+
   render() {
     console.log(this.props)
     return (
       <div>
         <p onClick={this.click}>this is topic list</p>
-        <p>{this.props.appStore.AppState.msg}</p>
+        <button onClick={this.login}>login</button>
+        <button onClick={this.mark}>mark all</button>
       </div> 
     )
   }
